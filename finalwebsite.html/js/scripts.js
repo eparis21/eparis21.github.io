9926556fe9d98db2f51357a49910a219420d713c
eparis21 <eparis@carroll.edu>
2024-04-27 15:11:19 -0600

    Final website

---
+++ finalwebsite.html/js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const hamMenu= document.querySelector('.ham-menu');
+  const offScreenMenu = document.querySelector('.off-screen-menu');
+
+  hamMenu.addEventListener('click', function() {
+      offScreenMenu.classList.toggle('active');
+  });
+});
